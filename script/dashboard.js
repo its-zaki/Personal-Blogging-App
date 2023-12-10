bc5035e8114e9915020766509add8d04f21073ac
its zaki <zakiyounus789@gmail.com>
2023-12-11 02:52:53 +0500

    push

--- script/dashboard.js
+++ script/dashboard.js
@@ -7,10 +7,15 @@ import {
   collection,
   addDoc,
   getDocs,
+  query,
+  orderBy,
+  where,
+  Timestamp,
 } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const logout = document.querySelector("#logout");
 
+console.log(auth);
 onAuthStateChanged(auth, (user) => {
   if (user) {
     const uid = user.uid;
@@ -37,28 +42,68 @@ logout.addEventListener("click", () => {
 const form = document.querySelector("#form3");
 const title = document.querySelector("#TITLE");
 const desc = document.querySelector("#DESC");
+const main = document.querySelector("#box");
 let arr = [];
 
 // function render (){
 
 // }
+async function getdatafromfirestore() {
+  const q = await query(
+    collection(db, "posts"),
+    orderBy("postDate", "desc"),
+    
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    arr.length = 0;
+    arr.push(doc.data());
+    arr.map((item) => {
+      console.log(item);
+      main.innerHTML += ` <div id="main"><div class="container">
+      <div class="user-img">
+          <img src="./assets/img/profile.jpg" alt="user-img">
+      </div>
+      <div class="heads">
+          <h3>${item.title}</h3>
+          <span>username</span> -
+          <span>August 16th, 2023</span>
+      </div>
+    </div>
+    <div>
+      <p class="content">${item.desc}</p>
+    </div>
+    <div class="crud">
+      <button class="crud-btn">Delete </button> <button class="crud-btn">Edit</button>
+    </div> </div>`;
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  });
+}
+getdatafromfirestore();
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   if (title.value && desc.value !== "") {
+    main.innerHTML = "";
     try {
       // arr.push();
       const docRef = await addDoc(collection(db, "posts"), {
         title: title.value,
         desc: desc.value,
         uid: auth.currentUser.uid,
+        postDate: Timestamp.fromDate(new Date()),
       });
-      arr.push(title.value, desc.value)
+      arr.push(title.value, desc.value);
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
     title.value = "";
     desc.value = "";
+
+    getdatafromfirestore();
   } else {
     Swal.fire({
       icon: "error",
@@ -66,29 +111,4 @@ form.addEventListener("submit", async (event) => {
       text: "please enter any title or description",
     });
   }
-
 });
-async function getdatafromfirestore() {
-  const querySnapshot = await getDocs(collection(db, "posts"));
-  querySnapshot.forEach((doc) => {
-    const main = document.querySelector("#box");
-    main.innerHTML += ` <div id="main"><div class="container">
-    <div class="user-img">
-        <img src="./assets/img/profile.jpg" alt="user-img">
-    </div>
-    <div class="heads">
-        <h3>${doc.data().title}</h3>
-        <span>username</span> -
-        <span>August 16th, 2023</span>
-    </div>
-  </div>
-  <div>
-    <p class="content">${doc.data().desc}</p>
-  </div>
-  <div class="crud">
-    <button class="crud-btn">Delete </button> <button class="crud-btn">Edit</button>
-  </div> </div>`;
-    console.log(`${doc.id} => ${doc.data()}`);
-  });
-}
-getdatafromfirestore();
